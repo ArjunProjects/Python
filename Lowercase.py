36ad31ab542dd19fb1534cd554ba318b55bd7fa8
nagarjuna <nagarjunaserveforit@gmail.com>
2023-03-09 19:57:20 +0530

    Lowercase.py adding complete program

--- Lowercase.py
+++ Lowercase.py
@@ -1,1 +1,17 @@
-# test
+# Arrange string characters such that lowercase letters should come first
+# Given str = "PyNaTive"
+# Expected output: yaivePNT
+
+def lower_comes_first(act_str):
+    low = []
+    up = []
+    for ch in act_str:
+        if ch.islower():
+            low.append(ch)
+        else:
+            up.append(ch)
+    return "".join(low + up)
+
+
+res = lower_comes_first("PyNaTive");
+print(res)
