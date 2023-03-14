f6d3b03fa7a7c3cc15784b8b9fb5e11a4b5cbb43
nagarjuna <nagarjunaserveforit@gmail.com>
2023-03-14 19:56:35 +0530

    CalSumAndAvg.py updating second approach with regular exp

--- CalSumAndAvg.py
+++ CalSumAndAvg.py
@@ -2,6 +2,8 @@
 # Given str1 = "PYnative29@#8496"
 # Output Sum is: 38 Average is  6.333333333333333
 
+import re
+
 def cal_sum_avg(str):
     total = 0
     count = 0
@@ -14,4 +16,15 @@ def cal_sum_avg(str):
     return total/count
 
 
-print(cal_sum_avg("PYnative29@#8496"))
+#print(cal_sum_avg("PYnative29@#8496"))
+
+# another approach with regular expressions
+str1 = "PYnative29@#8496"
+
+digits = [int(num) for num in re.findall(r"\d", str1)]
+alphas = [str(ch) for ch in re.findall(r'[a-zA-Z]+', str1)]
+print(alphas)
+total = sum(digits)
+avg = total/len(digits)
+
+print(f"sum is {total} and avg is {avg}")
